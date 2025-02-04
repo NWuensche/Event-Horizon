d70d3360a0915d6ad20346eb7d589ec846a5b7d8
nwuensche <gh@nwuensche.com>
2025-02-04 17:41:59 +0100

    Sort

--- app/src/main/java/com/example/foregroundclock/MainVM.kt
+++ app/src/main/java/com/example/foregroundclock/MainVM.kt
@@ -20,7 +20,7 @@ class MainVM(private val context: android.content.Context): ViewModel() {
     var eventsDB = emptyList<EventDB>()
         set(value) {
             mainModel.saveEvents(value)
-            events.value = value.map { it.toEventView() }
+            events.value = value.map { it.toEventView() }.sortedBy { it.nextRepeat }
             field = value
         }
     val showDialog = MutableStateFlow(false)
